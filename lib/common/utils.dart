da088390f002a0085cfacdcd827d3444f9ee36df
Yash-Gupta001 <gyash4963@gmail.com>
2025-03-20 10:41:15 +0530

    upcoming movies model created

--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 const apikey = "";
 const kBackgroundColor = Colors.black;
 const Imageurl = "";
-const baseUrl = "https://";
 
 
 // https://www.themoviedb.org/
--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,8 +1,13 @@
 import 'package:netflix_clone_using_tmdb_api/common/utils.dart';
+import 'package:netflix_clone_using_tmdb_api/models/upcoming_movie_model.dart';
 
+const baseUrl = "https://";
 var key = "?api_key=$apikey";
 late String endPoint;
 
 class ApiServices {
-  
+  Future<UpcomingMovieModel> getUpcomingMovies() async{
+    endPoint="movie/upcoming";
+    final url = "$baseUrl$endPoint";
+  }
 }
